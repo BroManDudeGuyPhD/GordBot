7c309597e9a44fe1aa9e7cf5c8f4cda8855cb380
STORMTROOPER\Andrew <andrew.fossier@gmail.com>
2020-04-10 15:10:58 -0500

    Added shutdown command

--- src/main/java/bromandudeguyphd/gordbot/GordBot.java
+++ src/main/java/bromandudeguyphd/gordbot/GordBot.java
@@ -12,14 +12,22 @@ import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
 import com.sedmelluq.discord.lavaplayer.track.playback.NonAllocatingAudioFrameBuffer;
+import discord4j.common.jackson.Possible;
 import discord4j.core.DiscordClient;
 import discord4j.core.DiscordClientBuilder;
 import discord4j.core.event.domain.lifecycle.ReadyEvent;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.VoiceState;
+import discord4j.core.object.data.stored.PresenceBean;
 import discord4j.core.object.entity.Member;
 import discord4j.core.object.entity.User;
+import discord4j.core.object.presence.Activity;
 import discord4j.voice.AudioProvider;
+import discord4j.core.object.presence.Presence;
+import static discord4j.core.object.presence.Presence.online;
+import discord4j.gateway.json.StatusUpdate;
+import discord4j.gateway.json.StatusUpdate.Game;
+import discord4j.gateway.json.dispatch.PresenceUpdate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
@@ -66,30 +74,32 @@ public class GordBot {
     final DiscordClient gordbot = new DiscordClientBuilder(tokens.discordToken()).build();
         gordbot.getEventDispatcher().on(ReadyEvent.class)
             .subscribe(ready -> System.out.println("Logged in as " + ready.getSelf().getUsername()));
-
         
+    
         commands.put("commands", event -> event.getMessage().getChannel() 
             .flatMap(channel -> channel.createMessage("Not much here yet!"))
             .then());
-        
+  
         
         commands.put("ping", event -> event.getMessage().getChannel()
             .flatMap(channel -> channel.createMessage("Pong!"))
             .then());
         
+        
+        commands.put("shutdown",  event -> event.getMessage().getChannel()
+            .flatMap(channel -> channel.createMessage("Goodbye!").and(event.getMessage().delete().and(gordbot.logout())))
+            .then());
+        
 
-        
-        
 //Music Commands
         commands.put("join", event -> Mono.justOrEmpty(event.getMember())
             .flatMap(Member::getVoiceState)
             .flatMap(VoiceState::getChannel)
             // join returns a VoiceConnection which would be required if we were
             // adding disconnection features, but for now we are just ignoring it.
-            .flatMap(channel -> channel.join(spec -> spec.setProvider(provider)))
-            .then(event.getMessage().delete())
+            .flatMap(channel -> channel.join(spec -> spec.setProvider(provider)).and(event.getMessage().delete()))
             .then());
-        
+
         
         
         final TrackScheduler scheduler = new TrackScheduler(player);
@@ -104,19 +114,18 @@ public class GordBot {
         gordbot.getEventDispatcher().on(MessageCreateEvent.class)
             .flatMap(event -> Mono.justOrEmpty(event.getMessage().getContent())
             .flatMap(content -> Flux.fromIterable(commands.entrySet())
-            // We will be using ! as our "prefix" to any command in the system.
-            .filter(entry -> content.startsWith('!' + entry.getKey()))
+            // We will be using . as our "prefix" to any command in the system.
+            .filter(entry -> content.startsWith('.' + entry.getKey()))
             .flatMap(entry -> entry.getValue().execute(event))
             .next()))
         .subscribe();
         
-       
         
-
-        //Where the bot logs on A.K.A ====> MUST RUN LAST <====
+           
+//Where the bot logs on A.K.A ====> MUST RUN LAST <====
         gordbot.login().block();
         
-    
+        
 //        try {
 //            bootSkype();
 //        }catch (InvalidCredentialsException | ConnectionException | NotParticipatingException ignored){}
