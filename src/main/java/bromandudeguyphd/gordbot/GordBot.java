5c58e1d5a8e5a7acabf1455a6bfd0636b4b0ae54
Andrew <BroManDudeGuyPhD@users.noreply.github.com>
2021-02-08 21:44:01 -0600

    added ember to ping command. still working on dance mention

--- src/main/java/bromandudeguyphd/gordbot/GordBot.java
+++ src/main/java/bromandudeguyphd/gordbot/GordBot.java
@@ -12,6 +12,8 @@ import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
 import com.sedmelluq.discord.lavaplayer.track.playback.NonAllocatingAudioFrameBuffer;
+
+import discord4j.common.util.Snowflake;
 import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.guild.GuildCreateEvent;
@@ -26,7 +28,10 @@ import discord4j.core.object.entity.channel.MessageChannel;
 import discord4j.core.object.presence.Activity;
 import discord4j.voice.AudioProvider;
 import discord4j.core.object.presence.Presence;
+import discord4j.core.spec.MessageCreateSpec;
+import discord4j.rest.util.Color;
 
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 import reactor.core.publisher.Flux;
@@ -102,10 +107,39 @@ public class GordBot {
             .map(MessageCreateEvent::getMessage)
             .filter(message -> message.getAuthor().map(user -> !user.isBot()).orElse(false))
             .filter(message -> message.getUserMentionIds().contains(gordbot.getSelfId()))
-            .filter(message -> message.getContent().toLowerCase().contains("dance"))            
-            .flatMap(Message::getChannel)
-            .flatMap(channel -> channel.createMessage("DANCE"))
-        .subscribe();   
+            .filter(message -> message.getContent().toLowerCase().contains("dance"))          
+            //.flatMap(Message::getChannel)
+            //.flatMap(channel -> channel.createMessage("DANCE"))
+            
+            .doOnNext(event -> {
+                MessageCreateSpec messageBuilder = new MessageCreateSpec();
+                final String content = event.getContent().toLowerCase().replace("<@!697886793739010111> ", "");
+                // System.out.println(content.toString());
+                // event.addReaction(ReactionEmoji.of(null, "\uD83D\uDE80", false));    
+    
+                //event.getChannel().flatMap(channel -> channel.createMessage("HI"));
+    
+                
+                    final Snowflake messageID = event.getChannelId();
+                    messageBuilder.setMessageReference(messageID);
+    
+    
+                    if (content.contains("dance")){ //"https://sirbrobot.com/images/dancingKnight.gif"
+                        
+                        messageBuilder.setContent("https://sirbrobot.com/images/dancingKnight.gif");
+                    }
+                    else if (content.contains("test")){
+                        messageBuilder.setContent("test!");
+                    }
+                    else{
+                    //Implement NLP here later. At the moment bot ignores non specified mentions
+                        messageBuilder.setContent("what?!");
+                    }  
+           
+            })
+            .subscribe();
+            
+        //.subscribe();  
 
 //Commands
         commands.put("commands", event -> event.getMessage().getChannel() 
@@ -121,8 +155,23 @@ public class GordBot {
                     ""))
             .then());
         
-        commands.put("ping", event -> event.getMessage().getChannel()
-            .flatMap(channel -> channel.createMessage("Pong!"))
+        commands.put("embed", event -> event.getMessage().getChannel()
+            .flatMap(channel -> channel.createEmbed(spec -> 
+            spec.setColor(Color.RED)
+              .setAuthor("setAuthor", "https://www.youtube.com/watch?v=Gc2u6AFImn8", "https://cdn.betterttv.net/emote/55028cd2135896936880fdd7/3x")
+              .setImage("https://cdn.betterttv.net/emote/55028cd2135896936880fdd7/3x")
+              .setTitle("setTitle/setUrl")
+              .setUrl("https://www.youtube.com/watch?v=Gc2u6AFImn8")
+              .setDescription("setDescription\n" +
+                "big D: is setImage\n" +
+                "small D: is setThumbnail\n" +
+                "<-- setColor")
+              .addField("addField", "inline = true", true)
+              .addField("addFIeld", "inline = true", true)
+              .addField("addFile", "inline = false", false)
+              .setThumbnail("https://cdn.betterttv.net/emote/55028cd2135896936880fdd7/3x")
+              .setFooter("setFooter --> setTimestamp", "https://cdn.betterttv.net/emote/55028cd2135896936880fdd7/3x")
+              .setTimestamp(Instant.now())))
             .then());
         
         commands.put("uptime", event -> event.getMessage().getChannel()
